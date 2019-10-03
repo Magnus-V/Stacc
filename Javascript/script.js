e4cfc719d48af7fc37ea8e07bd317516a2c6935b
Magnus-V <magnves@gmail.com>
2019-10-03 14:43:06 +0200

    Test

---
+++ Javascript/script.js
@@ -0,0 +1,14 @@
+$("button").click(function(){
+  $.post("demo_test_post.asp",
+  {
+    name: "Donald Duck",
+    city: "Duckburg"
+  },
+  function(data, status){
+    alert("Data: " + data + "\nStatus: " + status);
+  });
+});
+
+$('.datepicker').datepicker({
+    format: 'yyyy-mm-dd'
+});
