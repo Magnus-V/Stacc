17bf0eed831bc4ecbfb8602c6c62829992775a57
Magnus-V <magnves@gmail.com>
2019-10-03 20:03:22 +0200

    YO

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,14 +1,34 @@
-$("button").click(function(){
-  $.post("demo_test_post.asp",
-  {
-    name: "Donald Duck",
-    city: "Duckburg"
-  },
+
+var gURL = "https://visningsrom.stacc.com/dd_server_laaneberegning/rest/laaneberegning/v1/nedbetalingsplan"
+
+
+$('#form').submit(function(event){
+  event.preventDefault();
+  var vUkjentVerdi="TERMINBELOP";
+  $.post(gURL,
+    JSON.stringify({
+        laanebelop:parseInt($('#laanebelop').val()),
+        nominellRente:parseInt($('#nominellRente').val()),
+        terminGebyr:parseInt($('#termingebyr').val()),
+        utlopsDato:$('#utlopsDato').val(),
+        saldoDato:$('#saldoDato').val(),
+        datoForsteInnbetaling:$('datoForsteInnbetaling'),
+        ukjentVerdi: vUkjentVerdi
+    }),
   function(data, status){
-    alert("Data: " + data + "\nStatus: " + status);
+    alert("Data: " + data[0] + "\nStatus: " + status);
+    console.log(data);
+    alert(status);
+    debugger;
   });
 });
 
+$( function() {
+  $( ".datepicker" ).datepicker({
+    dateFormat: 'yy-mm-dd'
+  });
+} );
+
 $('.datepicker').datepicker({
-    format: 'yyyy-mm-dd'
+
 });
