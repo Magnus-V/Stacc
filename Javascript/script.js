5650465b8f2ea6cec79e4c485a2d895940fd6941
Magnus Vestby <magnves@gmail.com>
2019-10-07 01:27:39 +0200

    v.03 Fungerende

--- Javascript/script.js
+++ Javascript/script.js
@@ -4,6 +4,7 @@ var gURL = "https://visningsrom.stacc.com/dd_server_laaneberegning/rest/laaneber
 
 function postRequest(){
   var vUkjentVerdi="TERMINBELOP";
+  var vInnbetaling;
   $.ajax({
     'url': 'https://visningsrom.stacc.com/dd_server_laaneberegning/rest/laaneberegning/v1/nedbetalingsplan',
     'method':'POST',
@@ -20,30 +21,124 @@ function postRequest(){
         ukjentVerdi: "TERMINBELOP"
     }),
     success: function(data, status, xhr){
-      doSomethingWithTheData(data);
-      return data;
 
       },
     error: function(xhr,status,error){
       console.log(xhr);
       console.log(status);
       console.log(error);
+    },
+    complete:function(xhr){
+      vInnbetaling = xhr.responseJSON.nedbetalingsplan.innbetalinger;
+      downPaymentChart(vInnbetaling);
     }
     });
 }
 
-function doSomethingWithTheData(data){
-  var vNedbetaling = data.nedbetalingsplan;
-  
-  console.log(vNedbetaling);
-}
 
 $('#submitButton').click(function(){
   postRequest();
 });
 
+function downPaymentChart(pInnbetaling){
+  var vDato = [],
+  vGebyr = [],
+  vInnbetaling = [],
+  vRenter = [],
+  vRestgjeld = [],
+  vTotal = [];
+
+  pInnbetaling.forEach(function(item){
+    vDato.push(item.dato);
+    vGebyr.push(item.gebyr);
+    vInnbetaling.push(item.innbetaling)
+    vRenter.push(item.renter)
+    vRestgjeld.push(item.restgjeld)
+    vTotal.push(item.total);
+  });
+
+  var vMinValue, vMaxValue;
+  var vDatoer = [],
+  vFindMin = [];
+
+  function dataConverter(pDataArray){
+        var vTempArrayOfDates = [];
+
+        pDataArray.forEach(function(pDate){
+           var vTempDate = new Date(pDate);
+           vTempDate = vTempDate.getUTCFullYear() + '-' + (vTempDate.getUTCMonth() + 1);
+           if(vTempDate > 0){
+               vFindMin.push(vTempDate);
+           }
+           var vToPush = [vTempDate];
+           vTempArrayOfDates.push(vToPush);
+        });
+        vFindMin.sort();
+
+        return vTempArrayOfDates;
+    }
+  vDatoer = dataConverter(vDato);
+  vMaxValue = vFindMin[vDatoer.length-1];
+  vMinValue = vFindMin[0];
+  debugger;
+
+  Highcharts.chart('chartPayment', {
+    chart: {
+      type:'line',
+    },
+    title: {
+      text: 'Nedbetalingsplan',
+    },
+    subtitle:{
+
+    },
+    yAxis: {
+      title:{
+        text:'NOK',
+      },
+      valueDecimals: 2,
+    },
+    legend:{
+
+    },
+    tooltip: {
+        valueDecimals: 0,
+        valueSuffix: 'NOK'
+    },
+    xAxis:{
+      categories:vDatoer,
+      valueDecimals: 2,
+      tickInterval:2,
+    },
+    plotOptions: {
+    },
+    series: [
+      {
+        name: 'Gebyr',
+        data: vGebyr,
+      },
+      {
+        name:'Innbetaling',
+        data:vInnbetaling,
+      },
+      {
+        name: 'Renter',
+        data: vRenter,
+      },
+      {
+        name:'Restgjeld',
+        data:vRestgjeld,
+      },
+      {
+        name:'Total',
+        data:vTotal,
+      }
+    ],
+  })
+}
 $(function() {
   $( ".datepicker" ).datepicker({
-    dateFormat: 'yy-mm-dd'
+    dateFormat: 'yy-mm-dd',
+    uiLibrary: 'bootstrap4'
   });
 });
